287a5ae4980bd1ddb0d72e3616bbe2b5b44e9341
TalhaMehmood <talhamehmood991@gmail.com>
2023-11-18 00:56:02 +0500

    added cyclic.sh url

--- src/utils/AxiosConfiq.js
+++ src/utils/AxiosConfiq.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:4000',
+    baseURL: 'https://long-teal-basket-clam-ring.cyclic.app/',
     timeout: 10000,
 });
 
